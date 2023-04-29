f556a60653f18911bc4822ea0f5319a3061a0bd6
Nitish <nitish.kumar@indusnet.co.in>
2023-04-29 16:45:36 +0530

    show all uploaded data

--- clientproject/src/App.js
+++ clientproject/src/App.js
@@ -10,7 +10,8 @@ function App() {
   const getSingleFileslist = async () => {
     try {
       const fileslist = await getSingleFiles();
-      setSingleFiles(fileslist);
+      console.log("fileslist", fileslist);
+      setSingleFiles(fileslist.data);
     } catch (error) {
       console.log(error);
     }
@@ -18,8 +19,7 @@ function App() {
   const getMultipleFilesList = async () => {
     try {
       const fileslist = await getMultipleFiles();
-      setMultipleFiles(fileslist);
-      console.log(multipleFiles);
+      setMultipleFiles(fileslist.data);
     } catch (error) {
       console.log(error);
     }
@@ -43,48 +43,48 @@ function App() {
         <div className="row">
           <div className="col-6">
             <h4 className="text-success font-weight-bold">Single Files List</h4>
-            {/* <div className="row">
-              {singleFiles.map((file, index) => {
-                return (
+            <div className="row">
+              {singleFiles &&
+                singleFiles.map((item) => (
                   <div className="col-6">
                     <div className="card mb-2 border-0 p-0">
                       <img
-                        src={`http://localhost:8080/${file.filePath}`}
+                        src={`http://localhost:8080/${item.filePath}`}
                         height="200"
                         className="card-img-top img-responsive"
                         alt="img"
                       />
                     </div>
                   </div>
-                );
-              })}
-            </div> */}
+                ))}
+            </div>
           </div>
           <div className="col-6">
             <h4 className="text-success font-weight-bold">
               Multiple Files List
             </h4>
-            {/* {multipleFiles.map((element, index) => (
-              <div key={element._id}>
-                <h6 className="text-danger font-weight-bold">
-                  {element.title}
-                </h6>
-                <div className="row">
-                  {element.files.map((file, index) => (
-                    <div className="col-6">
-                      <div className="card mb-2 border-0 p-0">
-                        <img
-                          src={`http://localhost:8080/${file.filePath}`}
-                          height="200"
-                          className="card-img-top img-responsive"
-                          alt="img"
-                        />
+            {multipleFiles &&
+              multipleFiles.map((element, index) => (
+                <div key={element._id}>
+                  <h6 className="text-danger font-weight-bold">
+                    {element.title}
+                  </h6>
+                  <div className="row">
+                    {element.files.map((file, index) => (
+                      <div className="col-6">
+                        <div className="card mb-2 border-0 p-0">
+                          <img
+                            src={`http://localhost:8080/${file.filePath}`}
+                            height="200"
+                            className="card-img-top img-responsive"
+                            alt="img"
+                          />
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))} */}
+              ))}
           </div>
         </div>
       </div>
